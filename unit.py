cf2a98d54c3dc5610153ab08f3b503e00aae7ac0
sanhoeeko <806301104@qq.com>
2023-06-06 10:11:45 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -8,9 +8,10 @@ Order of basic SI units:
     mol [N]
 """
 
-import numpy as np
 import fractions as fr
 import re
+
+import numpy as np
 
 dic = 'TLMIKN'
 
@@ -21,6 +22,7 @@ _e = 1.602176634e-19  # Coulombs, the charge of an electron
 _kB = 1.380649e-23  # Joules per Kelvin, the Boltzmann constant
 _nA = 6.02214076e23  # inverse mol, the Avogadro constant
 _g = 6.6743e-11  # gravity constant
+_me = 9.10938356e-31  # kilograms, the mass of an electron
 
 
 class Unit:
@@ -63,18 +65,49 @@ def basic_unit(idx: int):
 
 
 class Constant:
-    def __init__(self, name, unit, value):
+    def __init__(self, name, unit, value=1.0):
         self.name = name
         self.unit = unit
         self.value = value
+
+    def __mul__(self, o: 'Constant'):
+        return Constant('', self.unit * o.unit, self.value * o.value)
+
+    def __truediv__(self, o: 'Constant'):
+        return Constant('', self.unit / o.unit, self.value / o.value)
+
+    def __pow__(self, power: int):
+        return Constant('', self.unit ** power, self.value ** power)
 
     def __repr__(self):
         return self.name + ' = ' + str(self.value) + ' ' + str(self.unit)
 
 
+"""
+example:
+
+    harmonic = UnitSpace([hbar_, Constant('m',M), Constant('w',T**-1)])
+    print(harmonic.unit_convert(L))
+
+"""
+
+
 class UnitSpace:
     def __init__(self, constants: list[Constant]):
-        self.mat = np.vstack(list(map(lambda x: x.unit.vec, constants)))
+        mat = np.vstack(list(map(lambda x: x.unit.vec, constants)))
+        m, n = mat.shape
+        if m == n:
+            self.mat = mat
+        else:
+            omitted = np.all(mat == 0, axis=0)
+            if np.sum(
+                    omitted) != n - m: raise ValueError  # that is because of too many or few constants to form a space
+            self.mat = np.eye(n)
+            cnt = 0
+            for i in range(n):
+                if not omitted[i]:
+                    self.mat[i, :] = mat[cnt, :]
+                    cnt += 1
         self.U = np.linalg.inv(self.mat)
         self.dic = list(map(lambda x: x.name, constants))
         self.ccs = np.array(list(map(lambda x: x.value, constants)))  # means "convert constants"
@@ -91,21 +124,47 @@ class UnitSpace:
         new_val = co.value / convert_constant
         return Constant(co.name, new_unit, new_val)
 
+    def factor(self, si_unit: Unit):
+        res = self.value_convert(Constant('', si_unit))
+        res.value **= -1
+        return res
+
+    def unit_to(self, origin: Unit, target: Unit):
+        """
+        show that what is the factor, that Origin * [Factor] = Target
+        """
+        factor = target / origin
+        return self.unit_convert(factor)
+
+    def value_to(self, origin: Constant, target: Unit):
+        factor_unit = target / origin.unit
+        factor = self.factor(factor_unit)
+        return origin * factor
+
 
 class IUnit(Unit):
     def __init__(self, vec: np.ndarray, base: UnitSpace):
         super(IUnit, self).__init__(vec)
         self.base = base
 
+    def si(self):
+        return Unit(self.vec @ self.base.mat)
+
     def __mul__(self, o: 'IUnit'):
         if self.base != o.base:
             raise ValueError
         return IUnit(self.vec + o.vec, self.base)
 
+    def __rmul__(self, o: Unit):
+        return o * self.si()
+
     def __truediv__(self, o: 'IUnit'):
         if self.base != o.base:
             raise ValueError
         return IUnit(self.vec - o.vec, self.base)
+
+    def __rdiv__(self, o: Unit):
+        return o / self.si()
 
     def __repr__(self):
         res = []
@@ -129,19 +188,31 @@ induced units
 """
 # CM & QM:
 Speed = L / T  # speed
+Frequency = T ** -1  # frequency
 Hamilton = M * Speed ** 2  # energy
-Action = Hamilton * T  # action / angular momentun
+Action = Hamilton * T  # action / angular momentum
 Area = L ** 2  # area
 Volume = L ** 3  # volume
 Dense = Volume ** -1  # particle density
+Momentum = M * Speed  # momentum
+Force = Momentum / T  # force
 Gravity = L ** 3 * M ** -1 * T ** -2
 
 # ED:
 Charge = I / Speed / Area / Dense  # charge
+Upotential = Hamilton / I  # electric potential
+Efield = Upotential / L  # electric field
+Dfield = Hamilton / Efield / Volume  # polarization field
+Bfield = Force / I / L  # magnetic field
+Mfield = Hamilton / Bfield / Volume  # magnetic polarization field
 
 # SM:
 Temp = K  # temperature
-Boltzmann = Hamilton / Temp  # kB
+Entropy = Hamilton / Temp  # entropy
+Boltzmann = Entropy / Temp  # kB
+Pressure = Hamilton / Volume  # pressure
+ChemicalPotential = Hamilton / Dense  # chemical potential
+HeatCapacity = Boltzmann  # heat capacity
 NA = N ** -1
 
 """
@@ -153,15 +224,55 @@ G_ = Constant('G', Gravity, _g)
 e_ = Constant('e', Charge, _e)
 kB_ = Constant('kB', Boltzmann, _kB)
 nA_ = Constant('NA', NA, _nA)
+me_ = Constant('me', M, _me)
 
 PlanckUnit = UnitSpace([c_, hbar_, G_, e_, kB_, nA_])
-NaturalMassUnit = UnitSpace([Constant('M', M, 1), c_, hbar_, e_, kB_, nA_])
-NaturalLengthUnit = UnitSpace([Constant('L', L, 1), c_, hbar_, e_, kB_, nA_])
+NaturalM = UnitSpace([Constant('M', M, 1), c_, hbar_, e_, kB_, nA_])
+NaturalL = UnitSpace([Constant('L', L, 1), c_, hbar_, e_, kB_, nA_])
+
+
+class Energy:
+    def __init__(self, si: float):
+        self.si = Constant('', Hamilton, si)
+
+    def ev(self):
+        return self.si.value / _e
+
+    def k(self):
+        return NaturalL.value_convert(self.si)
+
+    def wavelength(self):
+        return 1 / self.k().value
+
+    def freq(self):
+        return NaturalM.value_to(origin=self.si, target=Frequency)
+
+    def mass(self):
+        return NaturalM.value_to(origin=self.si, target=M)
+
+
+def energy_from_ev(ev: float):
+    return Energy(ev * _e)
+
+
+def energy_from_wavelength(lamda: float):
+    lamda = Constant('', L ** -1, 1 / lamda)
+    joule = NaturalL.value_to(origin=lamda, target=Hamilton).value
+    return Energy(joule)
+
+
+def energy_from_freq(freq: float):
+    freq = Constant('', Frequency, freq)
+    joule = NaturalM.value_to(origin=freq, target=Hamilton).value
+    return Energy(joule)
+
+
+def energy_from_mass(mass: float):
+    mass = Constant('', M, mass)
+    joule = NaturalM.value_to(origin=mass, target=Hamilton).value
+    return Energy(joule)
 
 
 if __name__ == '__main__':
-
-    # convert the well-known static energy 511keV to the mass of a electron
-
-    u = NaturalMassUnit.value_convert(Constant('E', Hamilton, 511e3*_e))
-    print(u)
+    test = energy_from_mass(_me)
+    print(test.ev())
